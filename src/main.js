8578d15773191f93a7314722717532fa0d75261f
fahmiin <fahmiin@tyne-solutions.com>
2020-06-28 22:58:35 +0800

    Added axios endpoint variants

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './man.css'
+import VueAxios from 'vue-axios'
+import { securedAxiosInstance, plainAxiosInstance } from './services/api.service'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, { secured: securedAxiosInstance, plain: plainAxiosInstance })
 
 new Vue({
   render: h => h(App),
